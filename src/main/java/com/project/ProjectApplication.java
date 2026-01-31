e4530cf5b2034565e495847e7ba152e4682d6df1
Sameer Kumar <sssameersingh0804@gmail.com>
2026-01-31 11:16:26 +0530

    Update ProjectApplication.java

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectApplication {
 
 	public static void main(String[] args) {
+		int x=10;
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
